cd82f20169040afaf3b987f2de9b271394252c62
olga-sokolova-ew <olga.sokolova@extrawest.com>
2022-07-05 13:14:40 +0200

    Adds notificationd through _document

---
+++ components/ui/Notification/Notification.js
@@ -0,0 +1,26 @@
+import ReactDOM from "react-dom";
+import classes from "./notification.module.css";
+
+const Notification = ({ title, message, status }) =>  {
+  let statusClasses = "";
+
+  if (status === "success") {
+    statusClasses = classes.success;
+  }
+
+  if (status === "error") {
+    statusClasses = classes.error;
+  }
+
+  const cssClasses = `${classes.notification} ${statusClasses}`;
+
+  return ReactDOM.createPortal(
+    <div className={cssClasses}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>,
+    document.getElementById("notifications") //id in _documents
+  );
+}
+
+export default Notification;
